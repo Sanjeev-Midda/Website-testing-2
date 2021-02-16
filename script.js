2a54ca258db6ed6df74bd972f13234e283885ad7
Learning <Learning@Sanjeevs-MacBook-Air.local>
2021-02-16 00:42:41 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+
+let button = document.getElementById("button");
+button.style = "background-color: red";
+
+var text = "This text has been updated!";
+
+
+document.write(text);
+
+alert("Hello World!");
+
+prompt("What's your name?");
+
+function amendedText () {
+    var name = 
+    //document.getElementById("title").value;
+
+}
